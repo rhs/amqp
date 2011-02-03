11ce848d0c12868f0513f6ca98979761321fc325
Rafael H. Schloming <rafaels@redhat.com>
2011-02-03 10:24:29 -0500

    added window-size to begin

--- session.py
+++ session.py
@@ -81,6 +81,7 @@ class Session:
     self.begin_sent = True
     self.post_frame(Begin(remote_channel = self.remote_channel,
                           next_outgoing_id = self.outgoing.unsettled_hwm + 1,
+                          window_size = 65536,
                           properties = self.properties))
 
   def do_begin(self, begin):
